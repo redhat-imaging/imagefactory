e8c2ea210635dcfb0a270a64bf6d01087751c2a1
Steve Loranz <sloranz@redhat.com>
2010-11-11 17:27:10 -0600

    initial commit

---
+++ basebuilder.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+
+class BaseBuilder(object):
+	"""docstring for BaseBuilder"""
+	def __init__(self, arg):
+		super(BaseBuilder, self).__init__()
+		self.arg = arg
+		
---
+++ tests/testbasebuilder.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+import unittest
+from basebuilder import BaseBuilder
+
+class BaseBuilderTestCase(unittest.TestCase):
+	def setUp(self):
+		pass
+    
+if __name__ == '__main__':
+	unittest.main()
