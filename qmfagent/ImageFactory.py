4bfd1c528e83d985e9b325650cfa1720ea8f68f8
Steve Loranz <sloranz@redhat.com>
2010-12-06 14:22:43 -0600

    Override __new__() to actually enforce the singleton pattern.

--- qmfagent/ImageFactory.py
+++ qmfagent/ImageFactory.py
@@ -22,10 +22,11 @@ import Build
 # Singleton representing the Factory itself
 
 class ImageFactory():
-
+	_instance = None
+	
     # ******* ImageFactory
     qmf_schema = qmf.SchemaObjectClass("com.redhat.imagefactory", "ImageFactory")
-
+	
     _method = qmf.SchemaMethod("build_image", {"desc":"Build a new image"})
     _method.add_argument(qmf.SchemaArgument("descriptor", qmf.TYPE_LSTR, {"dir":qmf.DIR_IN}))
     _method.add_argument(qmf.SchemaArgument("target", qmf.TYPE_SSTR, {"dir":qmf.DIR_IN}))
@@ -33,14 +34,19 @@ class ImageFactory():
     _method.add_argument(qmf.SchemaArgument("sec_credentials", qmf.TYPE_LSTR, {"dir":qmf.DIR_IN}))
     _method.add_argument(qmf.SchemaArgument("build", qmf.TYPE_REF, {"dir":qmf.DIR_OUT}))
     qmf_schema.add_method(_method)
-
-    qmf_agent = None 
-
+	
+    qmf_agent = None
+	
+	def __new__(cls):
+		if cls._instance is None:
+			cls._instance = cls.__new__(cls)
+		return cls._instance
+	
     def __init__(self):
         self.qmf_object = qmf.AgentObject(self.qmf_schema)
         _oid = self.qmf_agent.alloc_object_id()
         self.qmf_object.set_object_id(_oid)
-
+	
     def build_image(self,descriptor,target,image_uuid,sec_credentials):
         return Build.BuildAdaptor(descriptor,target,image_uuid,sec_credentials)
- 
+	
