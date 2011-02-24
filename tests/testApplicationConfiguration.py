ab98a603ffdef22afd069be4f29ac5365eb47892
Steve Loranz <sloranz@redhat.com>
2011-02-24 13:44:10 -0600

    updated copyright
    Signed-off-by: Steve Loranz <sloranz@redhat.com>

--- tests/testApplicationConfiguration.py
+++ tests/testApplicationConfiguration.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
-# Copyright (C) 2010 Red Hat, Inc.
+# Copyright (C) 2010-2011 Red Hat, Inc.
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
