redhat-imaging/imagefactory

commit 0e931dd35aa766141eb3355973d438e513390a10

Fixed a bug in the singleton pattern implementation that I found while working on another issue.
Signed-off-by: Steve Loranz <sloranz@redhat.com>

imgfac.py
@@ -2,16 +2,16 @@
 # encoding: utf-8
 
 # Copyright (C) 2010-2011 Red Hat, Inc.
-# 
+#
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; version 2 of the License.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
@@ -30,7 +30,7 @@ from imagefactory.ImageWarehouse import ImageWarehouse
 
 class Application(object):
     instance = None
-    
+
     def qmf_agent():
         doc = "The qmf_agent property."
         def fget(self):
@@ -41,26 +41,31 @@ class Application(object):
             del self._qmf_agent
         return locals()
     qmf_agent = property(**qmf_agent())
-    
-    
+
     def __new__(cls, *p, **k):
         if cls.instance is None:
-            cls.instance = object.__new__(cls, *p, **k)
-        return cls.instance
-    
-    def __init__(self):
-        super(Application, self).__init__()        
-        # logging.basicConfig(level=logging.NOTSET, format='%(asctime)s %(levelname)s %(name)s pid(%(process)d) Message: %(message)s')
-        self.daemon = False
-        self.pid_file_path = "/var/run/imagefactory.pid"
-        signal.signal(signal.SIGTERM, self.signal_handler)
-        self.app_config = ApplicationConfiguration().configuration
+            i = super(Application, self).__new__(cls, *p, **k)
+            # initialize here, not in __init__()
+            i.log = logging.getLogger('%s.%s' % (__name__, i.__class__.__name__))
+            i.daemon = False
+            i.pid_file_path = "/var/run/imagefactory.pid"
+            signal.signal(signal.SIGTERM, i.signal_handler)
+            i.app_config = ApplicationConfiguration().configuration
 
-        # by setting TMPDIR here we make sure that libguestfs
-        # (imagefactory -> oz -> libguestfs) uses the temporary directory of
-        # the user's choosing
-        os.putenv('TMPDIR', self.app_config['tmpdir'])
-    
+            # by setting TMPDIR here we make sure that libguestfs
+            # (imagefactory -> oz -> libguestfs) uses the temporary directory of
+            # the user's choosing
+            os.putenv('TMPDIR', self.app_config['tmpdir'])
+
+            cls.instance = i
+        elif(len(p) | len(k) > 0):
+            cls.instance.log.warn('Attempted re-initialize of singleton: %s' % (cls.instance, ))
+        return cls.instance
+
+    def __init__(self):
+        # logging.basicConfig(level=logging.NOTSET, format='%(asctime)s %(levelname)s %(name)s pid(%(process)d) Message: %(message)s')
+        pass
+
     def setup_logging(self):
         if (self.app_config['foreground']):
             logging.basicConfig(level=logging.WARNING, format='%(asctime)s %(levelname)s %(name)s pid(%(process)d) Message: %(message)s')
@@ -70,33 +75,32 @@ class Application(object):
             logging.getLogger('').setLevel(logging.DEBUG)
         elif (self.app_config['verbose']):
             logging.getLogger('').setLevel(logging.INFO)
-    
+
     def signal_handler(self, signum, stack):
         """docstring for sigterm_handler"""
         if (signum == signal.SIGTERM):
             logging.warn('caught signal SIGTERM, stopping...')
-            
+
             if (self.qmf_agent):
                 self.qmf_agent.shutdown()
                 try:
                     os.remove(self.pid_file_path)
                 except Exception, e:
                     self.log.warning(str(e))
-            
+
             sys.exit(0)
-    
-    
+
     # TODO: (redmine 273) - add code here to set the user:group we're running as and drop privileges
     def daemonize(self): #based on Python recipe 278731
         UMASK = 0
         WORKING_DIRECTORY = '/'
         IO_REDIRECT = os.devnull
-        
+
         try:
             pid = os.fork()
         except OSError, e:
             raise Exception, "%s [%d]" % (e.strerror, e.errno)
-        
+
         if (pid == 0):
             os.setsid()
             signal.signal(signal.SIGHUP, signal.SIG_IGN)
@@ -104,7 +108,7 @@ class Application(object):
                 pid = os.fork()
             except OSError, e:
                 raise Exception, "%s [%d]" % (e.strerror, e.errno)
-            
+
             if (pid == 0):
                 os.chdir(WORKING_DIRECTORY)
                 os.umask(UMASK)
@@ -112,25 +116,24 @@ class Application(object):
                 os._exit(0)
         else:
             os._exit(0)
-        
+
         for file_descriptor in range(0, 2): # close stdin, stdout, stderr
             try:
                 os.close(file_descriptor)
             except OSError:
                 pass # The file descriptor wasn't open to begin with, just ignore
-        
+
         os.open(IO_REDIRECT, os.O_RDWR)
         os.dup2(0, 1)
         os.dup2(0, 2)
-        
+
         return(True)
-    
-    
+
     def main(self):
         if (self.app_config['qmf']):
             if (not self.app_config['foreground']):
                 self.daemon = self.daemonize()
-            
+
             self.setup_logging()
             if(self.daemon):
                 try:
@@ -144,19 +147,19 @@ class Application(object):
                 logging.info("Launching in foreground...")
             else:
                 logging.warning("Failed to launch as daemon...")
-            
+
             self.qmf_agent = ImageFactoryAgent(self.app_config['qpidd'])
             self.qmf_agent.run()
-            
+
         else:
             self.app_config['foreground'] = True
             self.setup_logging()
-            
+
             if (self.app_config['template'] and self.app_config['target']):
                 self.builder = BuildDispatcher.builder_for_target_with_template(self.app_config['target'], self.app_config['template'])
                 self.builder_thread = BuildDispatcher.builder_thread_with_method(self.builder, 'build_image')
                 print("Image created with id: %s" % (self.builder.image_id, ))
-            
+
             elif (self.app_config['image'] and self.app_config['provider'] and self.app_config['credentials']):
                 credentials = self.app_config['credentials']
                 if(os.path.exists(credentials)):
@@ -164,19 +167,17 @@ class Application(object):
                     file_contents = credentials_file.read()
                     credentials_file.close()
                     credentials = file_contents
-                    
+
                 if(not (("<provider_credentials>" in file_contents.lower()) and ("</provider_credentials>" in file_contents.lower()))):
                     print("Unexpected content or formatting of credentials...")
                     sys.exit(1)
-                
+
                 warehouse = ImageWarehouse(self.app_config['warehouse'])
                 metadata = warehouse.metadata_for_id_of_type(('template', 'target'), self.app_config['image'], object_type='image')
                 self.builder = BuildDispatcher.builder_for_target_with_template(metadata['target'], metadata['template'])
                 kwargs = dict(image_id=self.app_config['image'], provider=self.app_config['provider'], credentials=credentials)
                 self.builder_thread = BuildDispatcher.builder_thread_with_method(self.builder, 'push_image', kwargs)
                 print("Image instance created with id: %s" % (self.builder.image_id, ))
-    
-
 
 if __name__ == "__main__":
     application = Application()

imagefactory/ApplicationConfiguration.py
@@ -22,10 +22,11 @@ import sys
 import os
 import argparse
 import json
+import logging
 
 class ApplicationConfiguration(object):
     instance = None
-    
+
     def configuration():
         doc = "The configuration property."
         def fget(self):
@@ -36,34 +37,38 @@ class ApplicationConfiguration(object):
             del self._configuration
         return locals()
     configuration = property(**configuration())
-    
-    
+
     def __new__(cls, *p, **k):
         if cls.instance is None:
-            cls.instance = object.__new__(cls, *p, **k)
+            i = super(ApplicationConfiguration, cls).__new__(cls, *p, **k)
+            #initialize here, not in __init__()
+            i.log = logging.getLogger('%s.%s' % (__name__, i.__class__.__name__))
+            i.configuration = {}
+            i.argparser = i.__new_argument_parser()
+            arguments = i.parse_arguments()
+
+            config_file_path = arguments.config
+            if (os.path.isfile(config_file_path)):
+                try:
+                    config_file = open(config_file_path)
+                    uconfig = json.load(config_file)
+                    # coerce this dict to ascii for python 2.6
+                    config = {}
+                    for k, v in uconfig.items():
+                        config[k.encode('ascii')]=v.encode('ascii')
+                    i.configuration = i.parse_arguments(defaults=config).__dict__
+                except IOError, e:
+                    i.log.exception(e)
+                    i.configuration = arguments
+
+            cls.instance = i
+        elif(len(p) | len(k) > 0):
+            cls.instance.log.warn('Attempted re-initialize of singleton: %s' % (cls.instance, ))
         return cls.instance
-    
+
     def __init__(self):
-        super(ApplicationConfiguration, self).__init__()
-        self.configuration = {}
-        self.argparser = self.__new_argument_parser()
-        arguments = self.parse_arguments()
-        
-        config_file_path = arguments.config
-        if (os.path.isfile(config_file_path)):
-            try:
-                config_file = open(config_file_path)
-                uconfig = json.load(config_file)
-                # coerce this dict to ascii for python 2.6
-                config = {}
-                for k, v in uconfig.items():
-                    config[k.encode('ascii')]=v.encode('ascii')
-                self.configuration = self.parse_arguments(defaults=config).__dict__
-            except IOError, e:
-                logging.exception(e)
-                self.configuration = arguments
-            
-    
+            pass
+
     def __new_argument_parser(self):
         main_description = """Image Factory is an application for creating system images to run virtual machines in various public and private \
                                 clouds.  The imgfac command can be used to start a daemon providing a QMFv2 agent interface, allowing for \
@@ -73,7 +78,7 @@ class ApplicationConfiguration(object):
         cli_build_description = """Build specified system and exit."""
         cli_push_description = """Instantiate an image and exit."""
         warehouse_description = """Options for specifying Image Warehouse (http://aeolusproject.org/imagewarehouse.html) base URL and bucket names."""
-        
+
         argparser = argparse.ArgumentParser(description=main_description, prog='imgfac')
         argparser.add_argument('--version', action='version', version='%(prog)s 0.1', help='Version info')
         argparser.add_argument('-v', '--verbose', action='store_true', default=False, help='Set verbose logging.')
@@ -99,9 +104,9 @@ class ApplicationConfiguration(object):
         group_warehouse.add_argument('--template_bucket', help='Name of warehouse bucket to look in for templates. (default: %(default)s)')
         group_warehouse.add_argument('--icicle_bucket', help='Name of warehouse bucket to look in for icicles. (default: %(default)s)')
         group_warehouse.add_argument('--provider_bucket', help='Name of warehouse bucket to look in for provider image instances. (default: %(default)s)')
-        
+
         return argparser
-    
+
     def parse_arguments(self, defaults=None):
         if(defaults):
             self.argparser.set_defaults(**defaults)
@@ -114,4 +119,3 @@ class ApplicationConfiguration(object):
             return self.argparser.parse_args('--image_bucket unittests_images --template_bucket unittests_templates --icicle_bucket unittests_icicles --provider_bucket unittests_provider_images'.split())
         else:
             return self.argparser.parse_args([])
-    

imagefactory/qmfagent/ImageFactory.py
@@ -30,7 +30,7 @@ import logging
 
 class ImageFactory(object):
     instance = None
-    
+
     # QMF schema for ImageFactory
     qmf_schema = Schema(SCHEMA_TYPE_DATA, "com.redhat.imagefactory", "ImageFactory")
     # method for building images
@@ -51,13 +51,12 @@ class ImageFactory(object):
     _states_method.addArgument(SchemaProperty("class_name", SCHEMA_DATA_STRING, direction=DIR_IN, desc="the name of the class to query for instance states"))
     _states_method.addArgument(SchemaProperty("states", SCHEMA_DATA_STRING, direction=DIR_OUT, desc="string representation of a dictionary describing the workflow"))
     qmf_schema.addMethod(_states_method)
-    
+
     @classmethod
     def object_states(cls):
         """Returns a dictionary representing the finite state machine for instances of this class."""
         return {}
-    
-    
+
     ## Properties
     def qmf_object():
         doc = "The qmf_object property."
@@ -69,40 +68,54 @@ class ImageFactory(object):
             del self._qmf_object
         return locals()
     qmf_object = property(**qmf_object())
-    
-    
+
+    def agent():
+        """The property agent"""
+        def fget(self):
+            return self._agent
+        def fset(self, value):
+            self._agent = value
+        def fdel(self):
+            del self._agent
+        return locals()
+    agent = property(**agent())
+
     def __new__(cls, *p, **k):
         if cls.instance is None:
-            cls.instance = object.__new__(cls, *p, **k)
+            i = super(ImageFactory, cls).__new__(cls, *p, **k)
+            # initialize here, not in __init__()
+            i.log = logging.getLogger('%s.%s' % (__name__, i.__class__.__name__))
+            i.qmf_object = Data(ImageFactory.qmf_schema)
+            #i.agent = k.get('agent', p[0] if (len(p) > 0) else None)
+            i.warehouse = ImageWarehouse(ApplicationConfiguration().configuration["warehouse"])
+            cls.instance = i
+        elif(len(p) | len(k) > 0):
+            cls.instance.log.warn('Attempted re-initialize of singleton: %s' % (cls.instance, ))
         return cls.instance
-    
-    def __init__(self, agent=None):
-        self.log = logging.getLogger('%s.%s' % (__name__, self.__class__.__name__))
-        self.qmf_object = Data(ImageFactory.qmf_schema)
-        self.agent = agent
-        self.warehouse = ImageWarehouse(ApplicationConfiguration().configuration["warehouse"])
-    
+
+    def __init__(self):
+        pass
+
     def image(self,template,target):
         template_object = Template(template=template)
         build_adaptor = BuildAdaptor.BuildAdaptor(template_object,target,self.agent)
         build_adaptor.build_image()
         return build_adaptor
-    
+
     def provider_image(self,image_id, provider, credentials):
         image_metadata = self.warehouse.metadata_for_id_of_type(("template", "target"), image_id, "image")
         template_id = image_metadata["template"]
         target = image_metadata["target"]
-        
+
         if (template_id and target):
             build_adaptor = BuildAdaptor.BuildAdaptor(Template(uuid=template_id),target,self.agent)
             build_adaptor.push_image(image_id, provider, credentials)
             return build_adaptor
         else:
             raise RuntimeError("Could not return build_adaptor!\nimage_metadata: %s\ntemplate_id: %s\ntemplate: %s\n" % (image_metadata, template_id, target))
-    
+
     def instance_states(self, class_name):
         """Returns a dictionary representing the finite state machine for instances of the class specified."""
         module_name = "imagefactory.qmfagent.%s" % (class_name, )
         __import__(module_name)
         return dict(states=str(getattr(sys.modules[module_name], class_name).object_states()))
-    

imagefactory/qmfagent/ImageFactoryAgent.py
@@ -69,7 +69,8 @@ class ImageFactoryAgent(AgentHandler):
         self.session.registerSchema(BuildAdaptor.qmf_event_schema_percentage)
         self.session.registerSchema(BuildAdaptor.qmf_event_schema_build_failed)
         # Now add the image factory object
-        self.image_factory = ImageFactory(agent=self)
+        self.image_factory = ImageFactory()
+        self.image_factory.agent = self
         self.image_factory_addr = self.session.addData(self.image_factory.qmf_object, "image_factory")
         self.log.info("image_factory has qmf/qpid address: %s", self.image_factory_addr)
     

tests/qmfagent/testImageFactory.py
@@ -43,11 +43,8 @@ class TestImageFactory(unittest.TestCase):
         image_factory_one = ImageFactory()
         image_factory_two = ImageFactory()
         self.assertIs(image_factory_one, image_factory_two)
-    
-    # def testClassDefinition(self):
-    #     pass
-    # 
-
+        del image_factory_one
+        del image_factory_two
 
 if __name__ == '__main__':
-	unittest.main()
+    unittest.main()

commit 4aef4f08b92f1fb3c0f42adbe6ff0d6db9fc95ae

Fix some errors in imgfac.py
Introduced by commit 0e931dd3

imgfac.py
@@ -44,7 +44,7 @@ class Application(object):
 
     def __new__(cls, *p, **k):
         if cls.instance is None:
-            i = super(Application, self).__new__(cls, *p, **k)
+            i = super(Application, cls).__new__(cls, *p, **k)
             # initialize here, not in __init__()
             i.log = logging.getLogger('%s.%s' % (__name__, i.__class__.__name__))
             i.daemon = False
@@ -55,7 +55,7 @@ class Application(object):
             # by setting TMPDIR here we make sure that libguestfs
             # (imagefactory -> oz -> libguestfs) uses the temporary directory of
             # the user's choosing
-            os.putenv('TMPDIR', self.app_config['tmpdir'])
+            os.putenv('TMPDIR', i.app_config['tmpdir'])
 
             cls.instance = i
         elif(len(p) | len(k) > 0):