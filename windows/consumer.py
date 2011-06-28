47456968fbce099a5949be2c9e0f98c9301b6168
ovisan <ovisan@redhat.com>
2011-06-28 13:42:31 +0200

    Changed the qpid code to send back messages to the client that request the work

--- windows/consumer.py
+++ windows/consumer.py
@@ -18,51 +18,24 @@
 # MA  02110-1301, USA.  A copy of the GNU General Public License is
 # also available at http://www.gnu.org/copyleft/gpl.html.
 
-from qpid.connection import Connection
-from qpid.datatypes import RangedSet, Message, uuid4
-from qpid.util import connect
 
-# Create connection and session
-socket = connect('localhost', 5672)
-connection = Connection(sock=socket, username='guest', password='guest')
-connection.start()
+from qpid.messaging import *
+from qpid.util import URL
+import base64
+from System.Diagnostics import Process
+
+connection = Connection('localhost', username='guest', password='guest')
+connection.open()
 session = connection.session(str(uuid4()))
 
-# Define local queue
-local_queue_name = 'local_queue'
-
-# Create local queue
-queue = session.incoming(local_queue_name)
-
-# Route messages from message_queue to my_local_queue
-session.message_subscribe(queue='client_queue', destination=local_queue_name)
-queue.start()
-
-content = ''
-
-#Setup proxy queue
-session.queue_declare(queue='proxy_queue')
-session.exchange_bind(exchange='amq.topic', queue='proxy_queue', binding_key='proxy_key')
-
-#Setup routing properties
-properties = session.delivery_properties(routing_key='proxy_key')
-
-output=''
+receiver = session.receiver('amq.topic')
 
 while True:
-    # Get message from the local queue
-    message = queue.get()
-    # Get body of the message
-    content = message.body
-    # Accept message (removes it from the queue)
-    session.message_accept(RangedSet(message.id))
-    # Print message content
-    #print content
-
-    from System.Diagnostics import Process
+    message = receiver.fetch()  
+    sender = session.sender(message.reply_to)
     proc = Process()
     proc.StartInfo.FileName = 'cmd.exe'
-    proc.StartInfo.Arguments = "/c %s"  % content
+    proc.StartInfo.Arguments = "/c %s"  % base64.b64decode(message.content)
     proc.StartInfo.UseShellExecute = False
     proc.StartInfo.RedirectStandardOutput = True
     proc.StartInfo.RedirectStandardError = True
@@ -70,7 +43,12 @@ while True:
     proc.WaitForExit()
     stdout = proc.StandardOutput.ReadToEnd()
     stderr = proc.StandardError.ReadToEnd()
-    session.message_transfer(destination='amq.topic', message=Message(properties, stdout))
+    message=Message(base64.b64encode(stdout) )
+    sender.send(message)
 
-session.close(timeout=10)
-# Close session
+
+
+session.acknowledge()
+connection.close()
+
+
--- windows/producer.py
+++ windows/producer.py
@@ -18,53 +18,29 @@
 # MA  02110-1301, USA.  A copy of the GNU General Public License is
 # also available at http://www.gnu.org/copyleft/gpl.html.
 
-from qpid.connection import Connection
-from qpid.datatypes import RangedSet, Message, uuid4
-from qpid.util import connect
+from qpid.messaging import *
+from qpid.util import URL
+import base64
 
 # Create connection and session
-socket = connect('ec2-50-17-149-29.compute-1.amazonaws.com', 5672)
-connection = Connection(sock=socket, username='Administrator', password='DRDqt-qR2?Z')
-connection.start()
+connection = Connection('broker', username='user', password='password')
+connection.open()
 session = connection.session(str(uuid4()))
 
-# Setup queue
-session.queue_declare(queue='client_queue')
-session.exchange_bind(exchange='amq.topic', queue='client_queue', binding_key='routing_key')
+sender = session.sender("amq.topic")
+receiver = session.receiver('reply-%s; {create:always, delete:always}' % session.name)
+messages = ["dir", "ipconfig", "netstat"]
 
-# Setup routing properties
-properties = session.delivery_properties(routing_key='routing_key')
+def send_message(command):
+    msg = Message(base64.b64encode(command))
+    msg.reply_to = 'reply-%s' % session.name
+    sender.send(msg)
+    message = receiver.fetch()
+    print base64.b64decode(message.content)
+    session.acknowledge()
 
-# Send messages
-session.message_transfer(destination='amq.topic', message=Message(properties, 'dir'))
-session.message_transfer(destination='amq.topic', message=Message(properties, 'ipconfig'))
-
-#TODO : implement also the listener here to receive messages back from the proxy
-
-
-# Define local queue
-local_queue_name = 'local_queue'
-
-# Create local queue
-queue = session.incoming(local_queue_name)
-
-# Route messages from message_queue to my_local_queue
-session.message_subscribe(queue='proxy_queue', destination=local_queue_name)
-queue.start()
-
-content = ''
-
-while (queue.qsize()) != 0:
-    # Get message from the local queue
-    message = queue.get(timeout=10)
-    # Get body of the message
-    content = message.body
-    # Accept message (removes it from the queue)
-    session.message_accept(RangedSet(message.id))
-    # Print message content
-    print content 
-
-# Close session
-session.close(timeout=100)
+for command in messages:
+    send_message(command)
 
 
+connection.close()
