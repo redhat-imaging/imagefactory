d24d8db0c8c819f41ba4b1a333843addcc6b254c
Chris Lalancette <clalance@redhat.com>
2011-08-04 13:53:30 -0400

    Replace a subprocess call with a native gzip call.
    Signed-off-by: Chris Lalancette <clalance@redhat.com>

--- imgfac/builders/FedoraBuilder.py
+++ imgfac/builders/FedoraBuilder.py
@@ -29,6 +29,7 @@ import httplib2
 import traceback
 import pycurl
 import json
+import gzip
 from cloudservers import CloudServers
 import ConfigParser
 import boto.ec2
@@ -1134,22 +1135,21 @@ class FedoraBuilder(BaseBuilder):
 
 
         # if the image is already here, great, otherwise grab it from the warehouse
-        input_image_path=self.app_config['imgdir'] + "/"
-        input_image_name="ec2-image-" + target_image_id + ".dsk"
-        input_image=input_image_path + input_image_name
+        input_image_name = "ec2-image-" + target_image_id + ".dsk"
+        input_image = self.app_config['imgdir'] + "/" + input_image_name
 
         self.retrieve_image(target_image_id, input_image)
 
         input_image_compressed_name = input_image_name + ".gz"
-        input_image_compressed=input_image + ".gz"
+        input_image_compressed = input_image + ".gz"
 
         if not os.path.isfile(input_image_compressed):
             self.log.debug("No compressed version of image file found - compressing now")
-            f_out = open(input_image_compressed, 'wb')
-            retcode = subprocess.call(['gzip', '-c', input_image], stdout=f_out)
+            f_in = open(input_image, 'rb')
+            f_out = gzip.open(input_image_compressed, 'wb')
+            f_out.writelines(f_in)
             f_out.close()
-            if retcode:
-                raise ImageFactoryException("Error while compressing image prior to scp")
+            f_in.close()
             self.log.debug("Compression complete")
 
         region=provider
