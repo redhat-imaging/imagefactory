f0651e7022baf4ec22bc07eb069906324d9d0185
Chris Lalancette <clalance@redhat.com>
2011-08-10 14:16:38 -0400

    Push the rhevm guest object down into build_upload.
    That is the only place it is needed.
    
    Signed-off-by: Chris Lalancette <clalance@redhat.com>

--- imgfac/builders/Fedora_rhevm_Builder.py
+++ imgfac/builders/Fedora_rhevm_Builder.py
@@ -44,19 +44,6 @@ class Fedora_rhevm_Builder(BaseBuilder):
         #   Oz now uses the tdlobject name property directly in several places so we must change it
         self.tdlobj.name = self.tdlobj.name + "-" + self.new_image_id
 
-        # populate a config object to pass to OZ; this allows us to specify our
-        # own output dir but inherit other Oz behavior
-        oz_config = ConfigParser.SafeConfigParser()
-        oz_config.read("/etc/oz/oz.cfg")
-        oz_config.set('paths', 'output_dir', self.app_config["imgdir"])
-
-        self.guest = oz.GuestFactory.guest_factory(self.tdlobj, oz_config, None)
-        self.guest.diskimage = self.app_config["imgdir"] + "/base-image-" + self.new_image_id + ".dsk"
-        # Oz assumes unique names - TDL built for multiple backends guarantees
-        # they are not unique.  We don't really care about the name so just
-        # force uniqueness
-        self.guest.name = self.guest.name + "-" + self.new_image_id
-
     def log_exc(self):
         self.log.debug("Exception caught in ImageFactory")
         self.log.debug(traceback.format_exc())
@@ -74,33 +61,46 @@ class Fedora_rhevm_Builder(BaseBuilder):
         self.log.debug("Building for target %s with warehouse config %s" % (self.target, self.app_config['warehouse']))
         self.status="BUILDING"
 
-        self.guest.cleanup_old_guest()
-        self.guest.generate_install_media(force_download=False)
+        # populate a config object to pass to OZ; this allows us to specify our
+        # own output dir but inherit other Oz behavior
+        oz_config = ConfigParser.SafeConfigParser()
+        oz_config.read("/etc/oz/oz.cfg")
+        oz_config.set('paths', 'output_dir', self.app_config["imgdir"])
+
+        guest = oz.GuestFactory.guest_factory(self.tdlobj, oz_config, None)
+        guest.diskimage = self.app_config["imgdir"] + "/base-image-" + self.new_image_id + ".dsk"
+        # Oz assumes unique names - TDL built for multiple backends guarantees
+        # they are not unique.  We don't really care about the name so just
+        # force uniqueness
+        guest.name = guest.name + "-" + self.new_image_id
+
+        guest.cleanup_old_guest()
+        guest.generate_install_media(force_download=False)
         self.percent_complete=10
 
         # We want to save this later for use by RHEV-M and Condor clouds
         libvirt_xml=""
 
         try:
-            self.guest.generate_diskimage()
+            guest.generate_diskimage()
             try:
                 # TODO: If we already have a base install reuse it
                 #  subject to some rules about updates to underlying repo
                 self.log.debug("Doing base install via Oz")
-                libvirt_xml = self.guest.install(self.app_config["timeout"])
-                self.image = self.guest.diskimage
+                libvirt_xml = guest.install(self.app_config["timeout"])
+                self.image = guest.diskimage
                 self.log.debug("Base install complete - Doing customization and ICICLE generation")
                 self.percent_complete = 30
-                self.output_descriptor = self.guest.customize_and_generate_icicle(libvirt_xml)
+                self.output_descriptor = guest.customize_and_generate_icicle(libvirt_xml)
                 self.log.debug("Customization and ICICLE generation complete")
                 self.percent_complete = 50
             except:
-                self.guest.cleanup_old_guest()
+                guest.cleanup_old_guest()
                 raise
         finally:
-            self.guest.cleanup_install()
+            guest.cleanup_install()
 
-        self.log.debug("Generated disk image (%s)" % (self.guest.diskimage))
+        self.log.debug("Generated disk image (%s)" % (guest.diskimage))
         # OK great, we now have a customized KVM image
         # Now we do some target specific transformation
 
