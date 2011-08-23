1644bf1d3238a9cd1a27cf78169350f316220ae4
Ian McLeod <imcleod@redhat.com>
2011-08-23 12:55:13 -0500

    Fix credentials decode for rackspace (mmorsi)

--- imgfac/builders/Fedora_rackspace_Builder.py
+++ imgfac/builders/Fedora_rackspace_Builder.py
@@ -106,7 +106,7 @@ class Fedora_rackspace_Builder(BaseBuilder):
         doc = libxml2.parseDoc(credentials)
 
         rack_username = doc.xpathEval("//provider_credentials/rackspace_credentials/username")[0].content
-        rack_access_key = doc.xpathEval("//provider_credentials/rackspace_credentials/access_key")[0].content
+        rack_access_key = doc.xpathEval("//provider_credentials/rackspace_credentials/password")[0].content
 
         cloudservers = CloudServers(rack_username, rack_access_key)
         cloudservers.authenticate()
