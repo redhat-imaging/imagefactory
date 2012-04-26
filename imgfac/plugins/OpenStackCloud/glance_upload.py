68894c729ff08b9ddb520fabac50d0a13ad5296e
Ian McLeod <imcleod@redhat.com>
2012-04-25 23:46:29 -0400

    Initial development content.
    README file and a standalone uploader function in Python.

---
+++ imgfac/plugins/OpenStackCloud/glance_upload.py
@@ -0,0 +1,31 @@
+from glance import client as glance_client
+from pprint import pprint
+
+def glance_upload(image_filename, creds = {'auth_url': None, 'password': None, 'strategy': 'noauth', 'tenant': None, 'username': None},
+                  host = "0.0.0.0", port = "9292", token = None):
+
+    image_meta = {'container_format': 'bare',
+     'disk_format': 'qcow2',
+     'is_public': True,
+     'min_disk': 0,
+     'min_ram': 0,
+     'name': 'Factory Test Image',
+     'properties': {'distro': 'rhel'}}
+
+
+    c = glance_client.Client(host=host, port=port,
+                             auth_tok=token, creds=creds)
+
+    image_data = open(image_filename, "r")
+
+    image_meta = c.add_image(image_meta, image_data)
+
+    image_data.close()
+
+    return image_meta['id']
+
+
+image_id = glance_upload("/root/base-image-f19e3f9b-5905-4b66-acb2-2e25395fdff7.qcow2")
+
+print image_id
+
