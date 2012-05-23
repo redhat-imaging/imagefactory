22f90658a1246ba180274ad97a308db580623491
Ian McLeod <imcleod@redhat.com>
2012-05-22 23:00:26 -0500

    Proof of concept drop of separate SRPM and setup.py for plugins

--- imagefactory-plugins/setup.py
+++ imagefactory-plugins/setup.py
@@ -18,35 +18,34 @@ import os
 import os.path
 import subprocess
 
-version_file_path = "imgfac/Version.py"
-version_file = open(version_file_path, 'w')
+#version_file_path = "imgfac/Version.py"
+#version_file = open(version_file_path, 'w')
 pkg_version = subprocess.check_output('/usr/bin/git describe | tr - _', shell=True).rstrip('\n')
-version_file.write('VERSION = "%s"' % pkg_version)
-version_file.close()
+#version_file.write('VERSION = "%s"' % pkg_version)
+#version_file.close()
 
-datafiles=[('share/man/man1', ['Documentation/man/imagefactory.1']),
-           ('/etc/imagefactory', ['imagefactory.conf']),
-           ('/etc/pki/imagefactory', ['cert-ec2.pem']),
-           ('/etc/sysconfig', ['imagefactory']),
-           ('/etc/logrotate.d', ['imagefactory']),
-           ('/etc/rc.d/init.d', ['scripts/imagefactory'])]
+#datafiles=[('share/man/man1', ['Documentation/man/imagefactory.1']),
+#           ('/etc/imagefactory', ['imagefactory.conf']),
+#           ('/etc/pki/imagefactory', ['cert-ec2.pem']),
+#           ('/etc/sysconfig', ['imagefactory']),
+#           ('/etc/logrotate.d', ['imagefactory']),
+#           ('/etc/rc.d/init.d', ['scripts/imagefactory'])]
 
 class sdist(_sdist):
-    """ custom sdist command to prepare imagefactory.spec file """
+    """ custom sdist command to prepare imagefactory-plugins.spec file """
     def run(self):
-        cmd = (""" sed -e "s/@VERSION@/%s/g" < imagefactory.spec.in """ % pkg_version) + " > imagefactory.spec"
+        cmd = (""" sed -e "s/@VERSION@/%s/g" < imagefactory-plugins.spec.in """ % pkg_version) + " > imagefactory-plugins.spec"
         os.system(cmd)
         _sdist.run(self)
 
-setup(name='imagefactory',
+setup(name='imagefactory-plugins',
       version=pkg_version,
-      description='Image Factory system image generation tool',
+      description='Default plugins for the Image Factory system image generation tool',
       author='Ian McLeod',
       author_email='imcleod@redhat.com',
       license='Apache License, Version 2.0',
       url='http://www.aeolusproject.org/imagefactory.html',
-      packages=['imgfac', 'imgfac.builders', 'imgfac.rest'],
-      scripts=['imagefactory'],
-      data_files = datafiles,
+      package_dir = {'imagefactory-plugins': ''},
+      packages=['imagefactory-plugins.EC2Cloud', 'imagefactory-plugins.FedoraOS'],
       cmdclass = {'sdist': sdist}
       )
