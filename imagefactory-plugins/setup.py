00b8ab48baeca2189665f7e454fa0a8735a2acfc
Ian McLeod <imcleod@redhat.com>
2012-06-04 16:28:25 -0400

    Various fixes to make the updated setup.py files work with Python 2.6

--- imagefactory-plugins/setup.py
+++ imagefactory-plugins/setup.py
@@ -18,9 +18,23 @@ import os
 import os.path
 import subprocess
 
+# Required for Python 2.6 backwards compat
+def subprocess_check_output(*popenargs, **kwargs):
+    if 'stdout' in kwargs:
+        raise ValueError('stdout argument not allowed, it will be overridden.')
+
+    process = subprocess.Popen(stdout=subprocess.PIPE, stderr=subprocess.STDOUT, *popenargs, **kwargs)
+    stdout, stderr = process.communicate()
+    retcode = process.poll()
+    if retcode:
+        cmd = ' '.join(*popenargs)
+        raise Exception("'%s' failed(%d): %s" % (cmd, retcode, stderr))
+    return (stdout, stderr, retcode)
+
 #version_file_path = "imgfac/Version.py"
 #version_file = open(version_file_path, 'w')
-pkg_version = subprocess.check_output('/usr/bin/git describe | tr - _', shell=True).rstrip('\n')
+(pkg_version, ignore, ignore) = subprocess_check_output('/usr/bin/git describe | tr - _', shell=True)
+pkg_version = pkg_version.rstrip('\n')
 #version_file.write('VERSION = "%s"' % pkg_version)
 #version_file.close()
 
