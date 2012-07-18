b94b0fe4cfe621374a72412e57832b91121069d9
Steve Loranz <sloranz@redhat.com>
2012-07-18 10:16:19 -0500

    Added resource path to request log line.
    Signed-off-by: Steve Loranz <sloranz@redhat.com>

--- imgfac/rest/RESTv2.py
+++ imgfac/rest/RESTv2.py
@@ -33,7 +33,10 @@ rest_api = Bottle(catchall=True)
 def log_request(f):
     def decorated_function(*args, **kwargs):
         if(ApplicationConfiguration().configuration['debug']):
-            log.debug('Handling %s HTTP %s REQUEST: %s' % (request.headers.get('Content-Type'), request.method, request.body.read()))
+            log.debug('Handling %s HTTP %s REQUEST for resource at %s: %s' % (request.headers.get('Content-Type'),
+                                                                              request.method,
+                                                                              request.path,
+                                                                              request.body.read()))
         return f(*args, **kwargs)
     decorated_function.__name__ = f.__name__
     return decorated_function
