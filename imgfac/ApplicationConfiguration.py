dfd1174a11b2c52a58f7062a14baf4347ea4ba2e
Steve Loranz <sloranz@redhat.com>
2012-11-16 16:01:57 -0600

    make the help text consistent with provider_image
    Signed-off-by: Steve Loranz <sloranz@redhat.com>

--- imgfac/ApplicationConfiguration.py
+++ imgfac/ApplicationConfiguration.py
@@ -102,7 +102,7 @@ class ApplicationConfiguration(Singleton):
 
             cmd_delete = subparsers.add_parser('delete', help='Delete an image.')
             cmd_delete.add_argument('id', help='UUID of the image to delete')
-            cmd_delete.add_argument('--provider', type=argparse.FileType(), help='A file containing the provider description.')
+            cmd_delete.add_argument('--provider', type=argparse.FileType(), help="A file containing the provider description or a string literal starting with '@' such as '@ec2-us-east-1'.")
             cmd_delete.add_argument('--credentials', type=argparse.FileType(), help='A file containing the provider credentials')
             cmd_delete.add_argument('--target', help='The name of the target cloud for which to customize the image.')
             cmd_delete.add_argument('--parameters')
