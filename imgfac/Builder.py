4caf6a28e75b19f25e424ad7be9fe9b994b94c11
Steve Loranz <sloranz@redhat.com>
2012-12-13 10:37:08 -0600

    add callbacks for image deletion
    Signed-off-by: Steve Loranz <sloranz@redhat.com>

--- imgfac/Builder.py
+++ imgfac/Builder.py
@@ -55,6 +55,7 @@ class Builder(object):
         self._target_image_cbws = [ ]
         self._provider_image = None
         self._provider_image_cbws = [ ]
+        self._deletion_cbws = []
         self.base_thread = None
         self.target_thread = None
         self.push_thread = None
@@ -394,6 +395,9 @@ class Builder(object):
 
         @return TODO
         """
+        if parameters and ('callbacks' in parameters):
+            # This ensures we have workers in place before any potential state changes
+            self._init_callback_workers(image_object, parameters['callbacks'], self._deletion_cbws)
 
         thread_name = str(uuid.uuid4())[0:8]
         thread_kwargs = {'provider':provider, 'credentials':credentials, 'target':target, 'image_object':image_object, 'parameters':parameters}
@@ -419,7 +423,6 @@ class Builder(object):
             self.pim.save_image(image_object)
             self.log.error("Exception encountered in _delete_image_on_provider thread")
             self.log.exception(e)
-# encoding: utf-8
-
-#   Copyright 2012 Red Hat, Inc.
-#
+        finally:
+            # We only shut the workers down after a known-final state change
+            self._shutdown_callback_workers(image_object, self._deletion_cbws)
