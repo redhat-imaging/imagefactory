bb694465408d9b3b51a81e39f70730239de76287
Dennis Kliban <dkliban@redhat.com>
2013-04-19 18:42:09 -0400

    added oauth and ssl support to the end to end test.

--- scripts/tests/e2eTest.py
+++ scripts/tests/e2eTest.py
@@ -10,6 +10,12 @@ import os
 import sys
 from tempfile import NamedTemporaryFile
 import requests
+from requests_oauthlib import OAuth1
+
+client_key = 'mock-key'
+client_secret = 'mock-secret'
+
+oauth = OAuth1(client_key, client_secret=client_secret)
 
 
 description = 'Attempts an end to end test of the imagefactory command line interface\
@@ -24,7 +30,7 @@ description = 'Attempts an end to end test of the imagefactory command line inte
 argparser = argparse.ArgumentParser()
 argparser.add_argument('datafile', type=argparse.FileType('r'))
 argparser.add_argument('--cmd', default='/usr/bin/imagefactory', help='Path to the imagefactory command. (default: %(default)s)')
-argparser.add_argument('--url', default='http://localhost:8075/imagefactory', help='URL of the imagefactory instance to test. (default: %(default)s)')
+argparser.add_argument('--url', default='https://localhost:8075/imagefactory', help='URL of the imagefactory instance to test. (default: %(default)s)')
 argparser.add_argument('-L', help='uses the local CLI to run the tests instead of the REST api interface. (default: %(default)s)', action='store_false', dest='remote')
 
 args = argparser.parse_args()
@@ -78,7 +84,7 @@ def create_base_image(template_args):
 
         if args.remote:
             payload = {'base_image': {'template': TDL}}
-            r = requests.post(args.url+'/base_images', data=json.dumps(payload), headers=requests_headers)
+            r = requests.post(args.url+'/base_images', data=json.dumps(payload), headers=requests_headers, auth=oauth, verify=False)
             base_image_output_str = r.text
         else:
             (base_image_output_str, ignore, ignore) = subprocess_check_output('%s --output json --raw base_image %s' % (args.cmd, template.name), shell=True)
@@ -87,7 +93,7 @@ def create_base_image(template_args):
         while(base_image_output_dict['status'] not in ('COMPLETE', 'COMPLETED', 'FAILED')):
             sleep(proc_chk_interval)
             if args.remote:
-                r = requests.get(args.url+'/base_images/'+base_image_id)
+                r = requests.get(args.url+'/base_images/'+base_image_id, auth=oauth, verify=False)
                 base_image_output_str = r.text
                 print "Checking status of %s" % (base_image_id,)
             else:
@@ -112,7 +118,7 @@ def build_push_delete(target, index):
             if args.remote:
                 payload = {'target_image': {'target': target}}
                 print "Creating a target image"
-                r = requests.post(args.url+'/base_images/'+base_image['id']+'/target_images', data=json.dumps(payload), headers=requests_headers)
+                r = requests.post(args.url+'/base_images/'+base_image['id']+'/target_images', data=json.dumps(payload), headers=requests_headers, auth=oauth, verify=False)
                 target_image_output_str = r.text
             else:
                 (target_image_output_str, ignore, ignore) = subprocess_check_output('%s --output json --raw target_image --id %s %s' % (args.cmd, base_image['id'], target), shell=True)
@@ -121,7 +127,7 @@ def build_push_delete(target, index):
             while(target_image_output_dict['status'] not in ('COMPLETE', 'COMPLETED', 'FAILED')):
                 sleep(proc_chk_interval)
                 if args.remote:
-                    r = requests.get(args.url+'/target_images/'+target_image_id)
+                    r = requests.get(args.url+'/target_images/'+target_image_id, auth=oauth, verify=False)
                     target_image_output_str = r.text
                 else:
                     (target_image_output_str, ignore, ignore) = subprocess_check_output('%s --output json --raw images \'{"identifier":"%s"}\'' % (args.cmd, target_image_id), shell=True)
@@ -146,7 +152,7 @@ def build_push_delete(target, index):
                             provider_file.write(str(provider['definition']))
                             if args.remote:
                                 payload = {'provider_image': {'target': target, 'provider': provider['name'], 'credentials': provider['credentials']}}
-                                r = requests.post(args.url+'/target_images/'+target_image_id+'/provider_images', data=json.dumps(payload), headers=requests_headers)
+                                r = requests.post(args.url+'/target_images/'+target_image_id+'/provider_images', data=json.dumps(payload), headers=requests_headers, auth=oauth, verify=False)
                                 provider_image_output_str = r.text
                             else:
                                 (provider_image_output_str, ignore, ignore) = subprocess_check_output('%s --output json --raw provider_image --id %s %s %s %s' % (args.cmd, target_image_id, provider['target'], provider_file.name, credentials_file.name), shell=True)
@@ -155,7 +161,7 @@ def build_push_delete(target, index):
                             while(provider_image_output_dict['status'] not in ('COMPLETE', 'COMPLETED', 'FAILED')):
                                 sleep(proc_chk_interval)
                                 if args.remote:
-                                    r = requests.get(args.url+'/provider_images/'+provider_image_id)
+                                    r = requests.get(args.url+'/provider_images/'+provider_image_id, auth=oauth, verify=False)
                                     provider_image_output_str = r.text
                                 else:
                                     (provider_image_output_str, ignore, ignore) = subprocess_check_output('%s --output json --raw images \'{"identifier":"%s"}\'' % (args.cmd, provider_image_id), shell=True)
@@ -170,7 +176,7 @@ def build_push_delete(target, index):
                                 if args.remote:
                                     
                                     print "Checking status of %s" % (base_image_id,)
-                                    r = requests.delete(args.url+'/provider_images/'+provider_image_id)
+                                    r = requests.delete(args.url+'/provider_images/'+provider_image_id, auth=oauth, verify=False)
                                 else:
                                     subprocess_check_output('%s --output json --raw delete %s --target %s --provider %s --credentials %s' % (args.cmd, provider_image_id, provider['target'], provider_file.name, credentials_file.name), shell=True)
                         finally:
@@ -195,14 +201,14 @@ while(test_index < test_count):
     sleep(proc_chk_interval)
 for target_image in target_images:
     if args.remote:
-        r = requests.delete(args.url+'/target_images/'+target_image['id'])
+        r = requests.delete(args.url+'/target_images/'+target_image['id'], auth=oauth, verify=False)
     else:
         subprocess_check_output('%s --output json --raw delete %s' % (args.cmd, target_image['id']), shell=True)
 
 for base_image in base_images:
     if args.remote:
         print "About to delete base image: %s" % (base_image['id'],)
-        r = requests.delete(args.url+'/base_images/'+base_image['id'])
+        r = requests.delete(args.url+'/base_images/'+base_image['id'], auth=oauth, verify=False)
     else:
         subprocess_check_output('%s --output json --raw delete %s' % (args.cmd, base_image['id']), shell=True)
 
