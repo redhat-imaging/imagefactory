e49af5b81e21ab6086932313b96315a90c2f1a6c
Ian McLeod <imcleod@redhat.com>
2014-07-07 09:28:39 -0500

    first cut at Rackspace upload functionality
    Rackspace uses a task-based upload scheme that, as of the time of this patch,
    is not part of upstream OpenStack.  To use it we have to make use of the
    pyrax Python binding.
    
    The "provider" in the case of Rackspace is expected to be one of the standard
    rackspace region codes.  Known regions supported right now are:
    
    ORD, DFW and SYD
    
    On the Factory CLI these should be specified as @ORD, etc.

--- imagefactory_plugins/Rackspace/Rackspace.py
+++ imagefactory_plugins/Rackspace/Rackspace.py
@@ -21,14 +21,18 @@ import subprocess
 import libxml2
 import traceback
 import ConfigParser
+import os
 from time import *
 from imgfac.ApplicationConfiguration import ApplicationConfiguration
 from imgfac.ImageFactoryException import ImageFactoryException
 from imgfac.CloudDelegate import CloudDelegate
+from imgfac.FactoryUtils import launch_inspect_and_mount, shutdown_and_close, remove_net_persist, create_cloud_info
 from novaclient.v1_1 import client
 from novaclient.exceptions import NotFound
 import oz.Fedora
-
+import pyrax
+import pyrax.exceptions
+import pyvhd
 
 def subprocess_check_output(*popenargs, **kwargs):
     if 'stdout' in kwargs:
@@ -51,7 +55,8 @@ class Rackspace(object):
         # Activity should be a one line human-readable string indicating the task in progress
         # We log it at DEBUG and also set it as the status_detail on our active image
         self.log.debug(activity)
-        self.active_image.status_detail['activity'] = activity
+        if self.active_image:
+            self.active_image.status_detail['activity'] = activity
 
     def __init__(self):
         # Note that we are now missing ( template, target, config_block = None):
@@ -62,6 +67,7 @@ class Rackspace(object):
         self.oz_config = ConfigParser.SafeConfigParser()
         self.oz_config.read("/etc/oz/oz.cfg")
         self.oz_config.set('paths', 'output_dir', self.app_config["imgdir"])
+        self.active_image = None
 
         if "rackspace" in config_obj.jeos_images:
             self.rackspace_jeos_amis = config_obj.jeos_images['rackspace']
@@ -288,7 +294,135 @@ class Rackspace(object):
                 self.log.debug('Successfully deleted Rackspace image (%s)' % rackspace_image_id)
         except Exception, e:
             raise ImageFactoryException('Failed to delete Rackspace image (%s) with error (%s)' % (rackspace_image_id,
-                                                                                                   str(e)))
+                                                                                             str(e)))
+
+    def push_image_to_provider(self, builder, provider, credentials, target, target_image, parameters):
+        # Our target_image is a Rackspace compatible VHD - we need to upload to cloudfiles
+        # then register with 
+        self.builder = builder
+        self.active_image = self.builder.provider_image
+        self.rackspace_decode_credentials(credentials)
+
+        rackspace_region = provider.upper()
+        if rackspace_region not in [ "DFW", "ORD", "SYD" ]:
+            raise ImageFactoryException("Unknown Rackspace provider/region specified")
+
+        # This is a module-global setting but I believe setting it here is thread-safe
+        # repeating this in multiple invocations should be harmless
+        pyrax.set_setting("identity_type", "rackspace")
+
+        if (not self.rackspace_username) or (not self.rackspace_api_key):
+            self.status = "FAILED"
+            raise ImageFactoryException("You must supply a username and api key to upload images to Rackspace")
+
+        try:
+            ctx = pyrax.create_context("rackspace", username=self.rackspace_username, 
+                                                    api_key=self.rackspace_api_key)
+            ctx.authenticate()
+        except pyrax.exceptions.AuthenticationFailed:
+            self.status = "FAILED"
+            raise ImageFactoryException("Authentication into Rackspace API failed")
+
+        cf = ctx.get_client("swift", rackspace_region)
+        imgs = ctx.get_client("image", rackspace_region)
+        upload_container="factory-images"
+
+        if not upload_container in cf.list_containers():
+            self.log.debug("Our default container name (%s) does not exist - creating it" % (upload_container))
+            cont = cf.create_container(upload_container)
+        else:
+            cont = cf.get(upload_container)
+
+        self.log.debug("Uploading target image to Rackspace cloudfiles - this may take some time")
+        obj_name = "factory-image-" + str(self.builder.provider_image.identifier)
+        cf.upload_file(cont, self.builder.target_image.data, obj_name=obj_name)
+
+        self.log.debug("Rereading container content to verify our new image is there")
+        objs = cont.get_objects()
+
+        imgobj = None
+        for obj in objs:
+            if obj.name == obj_name:
+                imgobj = obj
+                break
+        if not imgobj:
+            raise ImageFactoryException("Could not find our new image (%s) in container (%s)" % (obj_name, upload_container))
+
+        self.log.debug("Creating import task")
+        task = imgs.import_task(obj, cont, img_format="VHD", img_name=obj_name)
+
+        for i in range(1,360):
+            # This gets us the current status - yes, we are just recreating the task object
+            task = task.manager.get(task.id)
+            if task.status in [ "success", "failure" ]:
+                break
+            self.log.debug("Waiting for success or failure - %d/3600 - current status (%s)" % (i*10, task.status))
+            sleep(10)
+
+        if task.status == "failure":
+            raise ImageFactoryException("Rackspace image import failed with reason: %", (task.message))
+
+	if task.status == "success":
+	    new_img = imgs.find(name=obj_name)
+            self.builder.provider_image.identifier_on_provider = new_img.id
+            self.log.debug("Import finished - ID: %s - name: %s - status: %s - size: %s - tags: %s" % (new_img.id, new_img.name, new_img.status, new_img.size, new_img.tags))
+        else:
+            raise ImageFactoryException("Got to end of push with unexpected status (%s)" % task.status)
+
+
+    def builder_should_create_target_image(self, builder, target, image_id, template, parameters):
+        self.log.info('builder_should_create_target_image() called on Rackspace plugin - returning True')
+        return True
+
+    def builder_will_create_target_image(self, builder, target, image_id, template, parameters):
+        # Nothing really to do here
+        pass
+
+    def builder_did_create_target_image(self, builder, target, image_id, template, parameters):
+        self.log.info('builder_did_create_target_image() called in Rackspace plugin')
+        self.status="BUILDING"
+
+        # TODO: This is a convenience variable for refactoring - rename
+        self.new_image_id = builder.target_image.identifier
+
+        # TODO: More convenience vars - revisit
+        self.template = template
+        self.target = target
+        self.builder = builder
+        self.image = builder.target_image.data
+
+        # This lets our logging helper know what image is being operated on
+        self.active_image = self.builder.target_image
+
+        # Add the cloud-info file
+        self.modify_oz_filesystem()
+
+        self.log.info("Transforming image for use on Rackspace")
+        self.rackspace_transform_image()
+
+        self.percent_complete=100
+        self.status="COMPLETED"
+
+    def rackspace_transform_image(self):
+        # On entry the image points to our generic KVM raw image
+        # Convert to a Citrix complatible VHD and then update the image property
+        target_image = self.image + ".tmp.vhd"
+        self.log.debug("Converting raw kvm image (%s) to Rackspace compatible VHD image (%s)" % (self.image, target_image))
+        inimage = open(self.image,"r")
+        outimage = open(target_image,"w")
+        pyvhd.do_vhd_convert(inimage, outimage)
+        inimage.close()
+        outimage.close()
+        self.log.debug("VHD conversion complete")
+        os.unlink(self.image)
+        os.rename(target_image, self.image)
+
+    def modify_oz_filesystem(self):
+        self.log.debug("Doing further Factory specific modification of Oz image")
+        guestfs_handle = launch_inspect_and_mount(self.builder.target_image.data)
+        remove_net_persist(guestfs_handle)
+        create_cloud_info(guestfs_handle, self.target)
+        shutdown_and_close(guestfs_handle)
 
     def abort(self):
         # TODO: Make this progressively more robust
@@ -314,6 +448,22 @@ class Rackspace(object):
 
         self.rackspace_account_number = self._rackspace_get_xml_node(doc, "account_number")
         self.rackspace_username = self._rackspace_get_xml_node(doc, "username")
-        self.rackspace_password = self._rackspace_get_xml_node(doc, "password")
+
+        got_pw_or_key = False
+
+        try:
+            self.rackspace_password = self._rackspace_get_xml_node(doc, "password")
+            got_pw_or_key = True
+        except ImageFactoryException:
+            self.rackspace_password = None
+
+        try:
+            self.rackspace_api_key =  self._rackspace_get_xml_node(doc, "api_key")
+            got_pw_or_key = True
+        except ImageFactoryException:
+            self.rackspace_api_key = None
+
+        if not got_pw_or_key:
+            raise ImageFactoryException("You must provide either an api_key or password for Rackspace")
 
         doc.freeDoc()
