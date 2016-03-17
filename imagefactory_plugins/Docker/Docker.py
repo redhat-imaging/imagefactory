e9ec158a0b3b194b28844256f3a0f680eeb0665d
Ian McLeod <imcleod@redhat.com>
2016-03-16 20:09:20 -0500

    allow building Docker base images for non-x86_64
    Patch originally from Dennis Gilmore - lost as a result of poor merge
    management by Ian McLeod

--- imagefactory_plugins/Docker/Docker.py
+++ imagefactory_plugins/Docker/Docker.py
@@ -170,8 +170,6 @@ class Docker(object):
     def builder_should_create_target_image(self, builder, target, image_id, template, parameters):
         self.log.debug("builder_should_create_target_image called for Docker plugin - doing all our work here then stopping the process")
         tdlobj = oz.TDL.TDL(xmlstring=template.xml, rootpw_required=self.app_config["tdl_require_root_pw"])
-        if tdlobj.arch != "x86_64":
-            raise Exception("Docker plugin currently supports only x86_64 images")
         # At this point our input base_image is available as builder.base_image.data
         # We simply mount it up in libguestfs and tar out the results as builder.target_image.data
         wrap_metadata = parameter_cast_to_bool(parameters.get('create_docker_metadata', True))
@@ -255,11 +253,16 @@ class Docker(object):
                 raise Exception("No docker JSON template available for specified docker version (%s)" % (dockerversion))
             docker_json_template=self.docker_templates_dict[dockerversion]
 
+            arch = tdlobj.arch
+            if arch == "x86_64":
+                arch = "amd64"
+            elif arch == "armv7hl":
+                arch = "armhfp"
             tdict = { }
             tdict['commentstring'] = parameters.get('comment', 'Created by Image Factory')
             tdict['os'] = parameters.get('os', 'linux')
             tdict['createdtime'] = datetime.datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%SZ')
-            tdict['arch'] = "amd64"
+            tdict['arch'] = arch
             tdict['idstring'] = docker_image_id
             tdict['cmd'] = cmd
             tdict['env'] = env
